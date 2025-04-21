2350b770169cbce73c4180b03304d5a9197dd01e
god3v <sooon9254@naver.com>
2025-04-22 00:56:10 +0900

    init: kotlin spring project init

---
+++ ecommerce-adapter/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    kotlin("jvm")
+}
+
+group = "com.ecommerce"
+version = "0.0.1-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    runtimeOnly("com.mysql:mysql-connector-j")
+
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(21)
+}
