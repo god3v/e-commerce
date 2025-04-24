334e50d77505bbbb99afef1acf425effa0f8e51e
god3v <sooon9254@naver.com>
2025-04-24 22:15:35 +0900

    feat: 쿠폰 관련 Mock API 구현 및 Swagger 적용 #6 #7

---
+++ ecommerce-adapter/src/main/kotlin/com/ecommerce/adapter/in/web/controller/coupon/CouponController.kt
@@ -0,0 +1,59 @@
+package com.ecommerce.adapter.`in`.web.controller.coupon
+
+import com.ecommerce.adapter.`in`.web.dto.ApiResponse
+import com.ecommerce.adapter.`in`.web.dto.coupon.CouponResponse
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDateTime
+import java.util.Date
+
+@RestController
+@RequestMapping("/api/v1")
+class CouponController: CouponApiController {
+
+    @GetMapping("/users/{userId}/coupons")
+    override fun getCoupons(@PathVariable userId: Long): ResponseEntity<ApiResponse<List<CouponResponse>>> {
+
+        return ResponseEntity.ok().body(
+            ApiResponse.success(
+                listOf(CouponResponse(
+                    1L,
+                    "쿠폰",
+                    "정액",
+                    1000,
+                    1000,
+                    1000,
+                    false,
+                    LocalDateTime.now(),
+                    LocalDateTime.now(),
+                    LocalDateTime.now()
+                ))
+            )
+        )
+    }
+
+    @PostMapping("/users/{userId}/coupons")
+    override fun issueCoupon(@RequestBody couponId: Long): ResponseEntity<ApiResponse<CouponResponse>> {
+        return ResponseEntity.ok().body(
+            ApiResponse.success(
+                CouponResponse(
+                    1L,
+                    "쿠폰",
+                    "정액",
+                    1000,
+                    1000,
+                    1000,
+                    false,
+                    LocalDateTime.now(),
+                    LocalDateTime.now(),
+                    LocalDateTime.now()
+                )
+            )
+        )
+    }
+}
