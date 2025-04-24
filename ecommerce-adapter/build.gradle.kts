1d660c796edabd77bd2c22c317d3a912ed390748
god3v <sooon9254@naver.com>
2025-04-24 22:04:59 +0900

    chore: gradle 설정 변경 및 SpringBoot Class 패키지 이동

--- ecommerce-adapter/build.gradle.kts
+++ ecommerce-adapter/build.gradle.kts
@@ -15,6 +15,10 @@ dependencies {
     runtimeOnly("com.mysql:mysql-connector-j")
 
     testImplementation("org.jetbrains.kotlin:kotlin-test")
+    testImplementation("org.springframework.boot:spring-boot-testcontainers")
+    testImplementation("org.testcontainers:junit-jupiter")
+    testImplementation("org.testcontainers:mysql")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.test {
--- ecommerce-adapter/src/main/kotlin/com/ecommerce/adapter/EcommerceApplication.kt
+++ ecommerce-adapter/src/main/kotlin/com/ecommerce/adapter/EcommerceApplication.kt
@@ -1,4 +1,4 @@
-package com.ecommerce
+package com.ecommerce.adapter
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
